9b3783564a2cba28d4ebeb2e50e2c3eeb03983d5
arthanis <tothimre@designtech.hu>
2019-07-28 20:58:22 +0200

    Add dependencies, remove unused blocks, add sassLint

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
+import BootstrapVue from 'bootstrap-vue';
+import Axios from 'axios';
+
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+Vue.prototype.$http = Axios;
+
+Vue.use(BootstrapVue);
 
 new Vue({
   router,

7ae1a29216abd29fea358f16864c9cf32115f784
arthanis <tothimre@designtech.hu>
2019-07-30 01:00:07 +0200

    Add basic query in homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,30 @@
 <template>
-  <div>home content...</div>
+  <div>
+    <p>home content...</p>
+
+    <ul class="list-group">
+      <li class="list-group-item" v-for="song in songs" :key="song.id">
+        <p class="mb-0"><strong>#{{ song.id }}</strong> {{ song.performer}} - {{ song.title }}</p>
+        <p class="text-info">Rating: {{ song.rating }}</p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'home',
   components: {},
+  data() {
+    return {
+      songs: [],
+    };
+  },
+  created() {
+    this.$http.get('songs')
+      .then((res) => {
+        this.songs = res.data.songs;
+      });
+  },
 };
 </script>
